9f5df2a0d2a48999fd4d2984d8e0f57b32a75298
yangfengzzz <yangfengzzz@hotmail.com>
2024-08-07 15:11:01 +0800

    savew

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -7,13 +7,51 @@
 import pyPhysX as physx
 
 
+class PhysxSceneConfig:
+    gravity = physx.PxVec3(0, 0, -9.81)  # default gravity
+    bounceThreshold = 2.0  # relative velocity below this will not bounce
+    enablePCM = True  # Use persistent contact manifold solver for contact
+    enableTGS = True  # use TGS solver
+    enableCCD = False  # use continuous collision detection
+    enableEnhancedDeterminism = False  # improve determinism
+    enableFrictionEveryIteration = True  # better friction calculation, recommended for robotics
+    cpuWorkers = 0  # CPU workers, 0 for using main thread
+
+
 def main():
     def error_callback(code: physx.PxErrorCode, message: str, file: str, line: int):
         print(message)
 
     foundation = physx.PxCreateFoundation(physx.ErrorCallback(error_callback))
     engine = physx.PxCreatePhysics(foundation, physx.PxTolerancesScale())
+    physx.PxInitExtensions(engine)
 
+    scene_config = PhysxSceneConfig()
+    scene_desc = physx.PxSceneDesc(engine.getTolerancesScale())
+    scene_desc.gravity = scene_config.gravity
+    scene_desc.setFilterShaderGPU()
+    scene_desc.solverType = physx.PxSolverType.eTGS if scene_config.enableTGS else physx.PxSolverType.ePGS
+    scene_desc.bounceThresholdVelocity = scene_config.bounceThreshold
+    scene_desc.gpuDynamicsConfig = physx.PxGpuDynamicsMemoryConfig()
+
+    scene_flags = physx.PxSceneFlags()
+    if scene_config.enableEnhancedDeterminism:
+        scene_flags |= physx.PxSceneFlag.eENABLE_ENHANCED_DETERMINISM
+    if scene_config.enablePCM:
+        scene_flags |= physx.PxSceneFlag.eENABLE_PCM
+    if scene_config.enableCCD:
+        scene_flags |= physx.PxSceneFlag.eENABLE_CCD
+    if scene_config.enableFrictionEveryIteration:
+        scene_flags |= physx.PxSceneFlag.eENABLE_FRICTION_EVERY_ITERATION
+    scene_flags |= physx.PxSceneFlag.eENABLE_GPU_DYNAMICS
+    scene_flags |= physx.PxSceneFlag.eENABLE_DIRECT_GPU_API
+    if not scene_config.enablePCM:
+        scene_flags |= physx.PxSceneFlag.eENABLE_PCM
+    scene_desc.flags = scene_flags
+    scene_desc.cpuDispatcher = physx.PxDefaultCpuDispatcherCreate(scene_config.cpuWorkers)
+    scene_desc.cudaContextManager = mEngine->getCudaContextManager(device->cudaId)
+
+    scene = engine.createScene(scene_desc)
 
 if __name__ == '__main__':
     main()
--- src/py_scene.cpp
+++ src/py_scene.cpp
@@ -15,6 +15,78 @@ namespace nb = nanobind;
 using namespace nb::literals;
 using namespace physx;
 
+namespace {
+inline PxFilterFlags TypeAffinityIgnoreFilterShader(PxFilterObjectAttributes attributes0,
+                                                    PxFilterData filterData0,
+                                                    PxFilterObjectAttributes attributes1,
+                                                    PxFilterData filterData1,
+                                                    PxPairFlags& pairFlags,
+                                                    const void* constantBlock,
+                                                    PxU32 constantBlockSize) {
+    if (PxFilterObjectIsTrigger(attributes0) || PxFilterObjectIsTrigger(attributes1)) {
+        pairFlags = PxPairFlag::eTRIGGER_DEFAULT;
+        return PxFilterFlag::eDEFAULT;
+    }
+
+    // if top 16 bits of word3 are different, the shapes will never collide
+    // e.g. they are in different scenes
+    if ((filterData0.word3 & 0xffff0000) != (filterData1.word3 & 0xffff0000)) {
+        return PxFilterFlag::eKILL;
+    }
+
+    // if the lower 16 bits of word3 are the same (e.g. articulation id)
+    // if word2 has a matching bit (e.g. door and frame both set the same bit)
+    // the shapes will not collide (e.g. ignore collisions within each articulation)
+    if ((filterData0.word2 & filterData1.word2) && ((filterData0.word3 & 0xffff) == (filterData1.word3 & 0xffff))) {
+        return PxFilterFlag::eKILL;
+    }
+
+    // Otherwise, apply MuJoCo's collision model to word0 and word1
+    if ((filterData0.word0 & filterData1.word1) || (filterData1.word0 & filterData0.word1)) {
+        pairFlags = PxPairFlag::eCONTACT_DEFAULT | PxPairFlag::eNOTIFY_CONTACT_POINTS |
+                    PxPairFlag::eNOTIFY_TOUCH_PERSISTS | PxPairFlag::eNOTIFY_TOUCH_FOUND |
+                    PxPairFlag::eNOTIFY_TOUCH_LOST | PxPairFlag::ePRE_SOLVER_VELOCITY |
+                    PxPairFlag::ePOST_SOLVER_VELOCITY | PxPairFlag::eDETECT_CCD_CONTACT;
+
+        return PxFilterFlag::eDEFAULT;
+    }
+    return PxFilterFlag::eKILL;
+}
+
+inline PxFilterFlags TypeAffinityIgnoreFilterShaderGpu(PxFilterObjectAttributes attributes0,
+                                                       PxFilterData filterData0,
+                                                       PxFilterObjectAttributes attributes1,
+                                                       PxFilterData filterData1,
+                                                       PxPairFlags& pairFlags,
+                                                       const void* constantBlock,
+                                                       PxU32 constantBlockSize) {
+    if (PxFilterObjectIsTrigger(attributes0) || PxFilterObjectIsTrigger(attributes1)) {
+        pairFlags = PxPairFlag::eTRIGGER_DEFAULT;
+        return PxFilterFlag::eDEFAULT;
+    }
+
+    // if top 16 bits of word3 are different, the shapes will never collide
+    // e.g. they are in different scenes
+    if ((filterData0.word3 & 0xffff0000) != (filterData1.word3 & 0xffff0000)) {
+        return PxFilterFlag::eKILL;
+    }
+
+    // if the lower 16 bits of word3 are the same (e.g. articulation id)
+    // if word2 has a matching bit (e.g. door and frame both set the same bit)
+    // the shapes will not collide (e.g. ignore collisions within each articulation)
+    if ((filterData0.word2 & filterData1.word2) && ((filterData0.word3 & 0xffff) == (filterData1.word3 & 0xffff))) {
+        return PxFilterFlag::eKILL;
+    }
+
+    // Otherwise, apply MuJoCo's collision model to word0 and word1
+    if ((filterData0.word0 & filterData1.word1) || (filterData1.word0 & filterData0.word1)) {
+        pairFlags = PxPairFlag::eCONTACT_DEFAULT;
+        return PxFilterFlag::eDEFAULT;
+    }
+    return PxFilterFlag::eKILL;
+}
+}  // namespace
+
 void bindScene(nb::module_& m) {
     nb::enum_<PxFrictionType::Enum>(m, "PxFrictionType").value("ePATCH", PxFrictionType::Enum::ePATCH);
     nb::enum_<PxSolverType::Enum>(m, "PxSolverType")
@@ -102,6 +174,14 @@ void bindScene(nb::module_& m) {
             .def("setToDefault", &PxSceneDesc::setToDefault)
             .def("isValid", &PxSceneDesc::isValid)
             .def("getTolerancesScale", &PxSceneDesc::getTolerancesScale, nb::rv_policy::reference)
+            .def("setFilterShaderCPU",
+                 [](PxSceneDesc* desc) {
+                     desc->filterShader = TypeAffinityIgnoreFilterShader;
+                 })
+            .def("setFilterShaderGPU",
+                 [](PxSceneDesc* desc) {
+                     desc->filterShader = TypeAffinityIgnoreFilterShaderGpu;
+                 })
             .def_rw("gravity", &PxSceneDesc::gravity)
             .def_rw("simulationEventCallback", &PxSceneDesc::simulationEventCallback)
             .def_rw("contactModifyCallback", &PxSceneDesc::contactModifyCallback)
@@ -256,6 +336,12 @@ void bindScene(nb::module_& m) {
             .def("release", &PxPruningStructure::release)
             .def("getNbRigidActors", &PxPruningStructure::getNbRigidActors);
 
+    m.def(
+            "PxDefaultCpuDispatcherCreate",
+            [](PxU32 numThreads) {
+                return PxDefaultCpuDispatcherCreate(numThreads);
+            },
+            nb::rv_policy::reference);
     nb::class_<PxCpuDispatcher>(m, "PxCpuDispatcher").def("getWorkerCount", &PxCpuDispatcher::getWorkerCount);
     nb::class_<PxDefaultCpuDispatcher, PxCpuDispatcher>(m, "PxDefaultCpuDispatcher")
             .def_prop_rw("runProfiled", &PxDefaultCpuDispatcher::getRunProfiled,
