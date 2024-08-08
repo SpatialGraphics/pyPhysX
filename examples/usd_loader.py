78d382472d2d6a3bcc0f0303e1101bedf9097af9
yangfengzzz <yangfengzzz@hotmail.com>
2024-08-08 08:34:04 +0800

    save

--- examples/usd_loader.py
+++ examples/usd_loader.py
@@ -4,10 +4,25 @@
 #  personal capacity and am not conveying any rights to any intellectual
 #  property of any third parties.
 
-import pyPhysX as physx
-from utils import PhysxSceneConfig
-import pxr
+from utils import PhysicsEngineCPU
+from pxr import Usd, UsdPhysics
 
 
 def main():
-    a = pxr.UsdPhysics.MaterialAPI.GetDynamicFrictionAttr()
+    engine = PhysicsEngineCPU()
+    material = engine.creator.createMaterial(0, 0, 0)
+
+    source = "xxx.usd"
+    stage = Usd.Stage.Open(source, Usd.Stage.LoadAll)
+
+    for prim in stage.Traverse():
+        type_name = str(prim.GetTypeName())
+        # path = str(prim.GetPath())
+
+        if type_name == "PhysicsMaterialAPI":
+            try:
+                material_scheme = UsdPhysics.MaterialAPI(prim)
+                material.restitution = material_scheme.GetRestitutionAttr().Get()
+
+            except Exception as e:
+                print(f"Failed to parse physics scene: {e}")
